7bb69b3e47afa03f3cddc5a15e4f3f196cc6aee5
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-09-11 11:00:56 +0100

    * Error view loader for handling error display

--- CampaignBrowser/ErrorLoader.swift
+++ CampaignBrowser/ErrorLoader.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2018 Westwing GmbH. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ErrorLoader: NSObject {
+    
+    static let sharedInstance = ErrorLoader()
+    
+    var popupView : ErrorView!
+    
+    typealias RetryErrorCompletion = () -> ()
+    
+    private func showPopup(completion: @escaping RetryErrorCompletion) {
+        hidePopup()
+        
+        var vc = UIApplication.shared.keyWindow?.rootViewController
+        while let presentedViewController = vc!.presentedViewController {
+            vc = presentedViewController
+        }
+        
+        self.popupView = ErrorView(frame: vc!.view.frame)
+        vc!.view.addSubview(self.popupView)
+        vc!.view.bringSubview(toFront: self.popupView)
+        vc!.view.backgroundColor = UIColor.black
+        self.popupView.performRetry = {
+            completion()
+            self.hidePopup()
+        }
+    }
+    
+    func hidePopup() {
+        if popupView != nil && popupView.superview != nil {
+            self.popupView.removeFromSuperview()
+        }
+    }
+    
+    func isLoaderVisible() -> Bool {
+        return popupView != nil
+    }
+    
+    func genericInitialze(show: Bool, completion: @escaping RetryErrorCompletion) {
+        if show {
+            showPopup(completion: completion)
+        } else {
+            hidePopup()
+        }
+    }
+}
