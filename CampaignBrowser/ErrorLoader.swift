3c8641211e1eb08dbbe40db4011e246830cc623e
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-09-11 10:45:09 +0100

    * Added files

---
+++ CampaignBrowser/ErrorLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorLoader.swift
+//  CampaignBrowser
+//
+//  Created by Abubakar Oladeji on 11/09/2018.
+//  Copyright © 2018 Westwing GmbH. All rights reserved.
+//
+
+import Foundation
---
+++ CampaignBrowser/ErrorView.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorView.swift
+//  CampaignBrowser
+//
+//  Created by Abubakar Oladeji on 11/09/2018.
+//  Copyright © 2018 Westwing GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ErrorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
