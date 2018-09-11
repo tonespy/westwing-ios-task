30fbd79e5336390b7bbcf0fc18d773a307b6f6c3
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-09-11 10:45:23 +0100

    * Added extension for elevating view

--- CampaignBrowser/UIView+Extension.swift
+++ CampaignBrowser/UIView+Extension.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018 Westwing GmbH. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func elevate(elevation: Double) {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: elevation)
+        self.layer.shadowRadius = CGFloat(elevation)
+        self.layer.shadowOpacity = 0.24
+    }
+}
